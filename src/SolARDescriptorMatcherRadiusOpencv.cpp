ae631e043169ff227eecdf32951c7645f93c8871
BCOM\jroyan <jerome.royan@b-com.com>
2019-03-01 00:44:12 +0100

    Remove unuseful logs

--- src/SolARDescriptorMatcherRadiusOpencv.cpp
+++ src/SolARDescriptorMatcherRadiusOpencv.cpp
@@ -55,12 +55,6 @@ DescriptorMatcher::RetCode SolARDescriptorMatcherRadiusOpencv::match(
  
         if (desc1->getNbDescriptors() == 0 || desc2->getNbDescriptors() == 0)
             return DescriptorMatcher::RetCode::DESCRIPTOR_EMPTY;
- 
-        std::cout << "Ref fiducial" << std::endl;
-        std::cout << desc1->data() << std::endl;
-
-        std::cout << "Ref fiducial" << std::endl;
-        std::cout << desc1->data() << std::endl;
 
         std::vector<std::vector<cv::DMatch>> cv_matches;
         std::vector<cv::DMatch> good_matches;
