241daf05c2e1909be9b9bd34a349e11188a79993
granger35 <jerome.royan@b-com.com>
2020-12-17 23:17:46 +0100

    Rename and reorganize Fiducial Marker test

--- tests/SolARTest_ModuleOpenCV_FiducialMarker/SolARTest_ModuleOpenCV_FiducialMarker.pro
+++ tests/SolARTest_ModuleOpenCV_FiducialMarker/SolARTest_ModuleOpenCV_FiducialMarker.pro
@@ -3,7 +3,7 @@ QT       -= core gui
 CONFIG -= qt
 
 ## global defintions : target lib name, version
-TARGET = SolAROpenCVFiducialMarker
+TARGET = SolARTest_ModuleOpenCV_FiducialMarker
 VERSION=0.9.0
 
 DEFINES += MYVERSION=$${VERSION}
@@ -67,7 +67,7 @@ win32 {
 }
 
 configfile.path = $${TARGETDEPLOYDIR}/
-configfile.files = $${PWD}/SolAROpenCVFiducialMarker_conf.xml
+configfile.files = $${PWD}/SolARTest_ModuleOpenCV_FiducialMarker_conf.xml
 INSTALLS += configfile
 
 DISTFILES += \
--- tests/SolARTest_ModuleOpenCV_FiducialMarker/main.cpp
+++ tests/SolARTest_ModuleOpenCV_FiducialMarker/main.cpp
@@ -44,9 +44,9 @@ int main(int argc,char* argv[])
     /* this is needed in dynamic mode */
     SRef<xpcf::IComponentManager> xpcfComponentManager = xpcf::getComponentManagerInstance();
 
-    if(xpcfComponentManager->load("SolAROpenCVFiducialMarker_conf.xml")!=org::bcom::xpcf::_SUCCESS)
+    if(xpcfComponentManager->load("SolARTest_ModuleOpenCV_FiducialMarker_conf.xml")!=org::bcom::xpcf::_SUCCESS)
     {
-        LOG_ERROR("Failed to load the configuration file SolAROpenCVFiducialMarker_conf.xml")
+        LOG_ERROR("Failed to load the configuration file SolARTest_ModuleOpenCV_FiducialMarker_conf.xml")
         return -1;
     }
 
