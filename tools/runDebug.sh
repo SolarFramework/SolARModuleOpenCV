414e36cd29c1806b44ffac753909a215501de9ff
BCOM\nduong <nam-duong.duong@b-com.com>
2021-11-09 12:26:11 +0100

    fix run file for linux

--- tools/runDebug.sh
+++ tools/runDebug.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-export REMAKEN_PKG_ROOT=~/.remaken
-echo "REMAKEN_PKG_ROOT=$REMAKEN_PKG_ROOT"
-
-# Update configuration files by replacing win-cl-14.1 by linux in module paths
-sed -i 's/win-cl-14.1/linux-gcc/' $PWD/$1_conf.xml
+export XPCF_MODULE_ROOT=~/.remaken/packages/linux-gcc
+echo "XPCF_MODULE_ROOT=$XPCF_MODULE_ROOT"
 
 ld_library_path="./"
-for modulePath in $(grep -o "\$REMAKEN_PKG_ROOT.*lib" $1_conf.xml)
+for modulePath in $(grep -o "\$XPCF_MODULE_ROOT.*lib" $1_conf.xml)
 do
-   modulePath=${modulePath/"\$REMAKEN_PKG_ROOT"/${REMAKEN_PKG_ROOT}}
+   modulePath=${modulePath/"\$XPCF_MODULE_ROOT"/${XPCF_MODULE_ROOT}}
    if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/debug" ]]
    then
       ld_library_path=$ld_library_path:$modulePath/x86_64/shared/debug
    fi 
 done
 
+echo "LD_LIBRARY_PATH=$ld_library_path $1"
 LD_LIBRARY_PATH=$ld_library_path $1
-echo "LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+
 
