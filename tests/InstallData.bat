b060b144bf46de87355309f0aee717d857dca991
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-12-14 17:45:34 +0100

    Update artifactory URL for generic

--- tests/InstallData.bat
+++ tests/InstallData.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Download a AR device capture
-curl https://artifact.b-com.com/solar-generic-local/captures/hololens/bcomLab/loopDesktopA.zip -L -o loopDesktopA.zip
+curl https://repository.solarframework.org/generic/captures/hololens/bcomLab/loopDesktopA.zip -L -o loopDesktopA.zip
 powershell Expand-Archive loopDesktopA.zip -DestinationPath .\data -F
 del loopDesktopA.zip
 
